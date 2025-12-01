de3bac55947735df0bfa67424d4111947dcb9a34
Sam Mahoney <mahoney@cmui.co.uk>
2025-12-01 12:12:12 +0000

    move ubuntu wm in its own config

--- homemgr/mahoney/helios/default.nix
+++ homemgr/mahoney/helios/default.nix
@@ -2,53 +2,11 @@
   # TODO: work specific stuff
   imports = [
     "${homemgrModules}/common"
+    "${homemgrModules}/ubuntu-common"
   ];
 
   # Enable Home Manager self-management
   programs.home-manager.enable = true;
-
-  # Enable X session for i3
-  xsession.enable = true;
-
-  # i3 window manager config (as before—customize further)
-  xsession.windowManager.i3 = {
-    enable = true;
-    package = pkgs.i3;
-
-    config = {
-      modifier = "Mod4";
-      terminal = "gnome-terminal";
-      menu = "rofi -show drun";
-
-      keybindings = let
-        mod = config.xsession.windowManager.i3.config.modifier;
-      in {
-        "${mod}+Return" = "exec ${config.xsession.windowManager.i3.config.terminal}";
-        "${mod}+d" = "exec ${config.xsession.windowManager.i3.config.menu}";
-        "${mod}+Shift+q" = "kill";
-        "${mod}+Shift+e" = "exec i3-nagbar -t warning -m 'Exit i3?' -b 'Yes' 'i3-msg exit'";
-      };
-
-      bars = [
-        {
-          position = "top";
-          statusCommand = "${pkgs.i3status}/bin/i3status";
-        }
-      ];
-    };
-
-    extraConfig = ''
-      gaps inner 10
-      gaps outer 5
-    '';
-  };
-
-  # Packages for i3 ecosystem
-  home.packages = with pkgs; [
-    i3status
-    rofi
-    # Add more: feh, picom, etc.
-  ];
 
   # State version
   home.stateVersion = "24.11";
---
+++ modules/home-manager/programs/ubuntu-common/default.nix
@@ -0,0 +1,53 @@
+{
+  outputs,
+  userConfig,
+  pkgs,
+  ...
+}:
+{
+  # This file contains my ubuntu WM (i3) config
+
+  # Enable X session for i3
+  xsession.enable = true;
+  
+  # i3 window manager config (as before—customize further)
+  xsession.windowManager.i3 = {
+    enable = true;
+    package = pkgs.i3;
+
+    config = {
+      modifier = "Mod4";
+      terminal = "gnome-terminal";
+      menu = "rofi -show drun";
+
+      keybindings = let
+        mod = config.xsession.windowManager.i3.config.modifier;
+      in {
+        "${mod}+Return" = "exec ${config.xsession.windowManager.i3.config.terminal}";
+        "${mod}+d" = "exec ${config.xsession.windowManager.i3.config.menu}";
+        "${mod}+Shift+q" = "kill";
+        "${mod}+Shift+e" = "exec i3-nagbar -t warning -m 'Exit i3?' -b 'Yes' 'i3-msg exit'";
+      };
+
+      bars = [
+        {
+          position = "top";
+          statusCommand = "${pkgs.i3status}/bin/i3status";
+        }
+      ];
+    };
+
+    extraConfig = ''
+      gaps inner 10
+      gaps outer 5
+    '';
+  };
+  
+  # Packages for i3 ecosystem
+  home.packages = with pkgs; [
+    i3status
+    rofi
+    # Add more: feh, picom, etc.
+  ];
+
+}
